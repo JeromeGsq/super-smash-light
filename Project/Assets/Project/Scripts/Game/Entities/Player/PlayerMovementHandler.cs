eb0e71a7399be7eb1c315e01ba1ab4ec42364c84
Jérôme Ghesquiere <jerome.ghesquiere@gmail.com>
2019-01-20 18:18:31 +0100

    Merge in one file

--- Project/Assets/Project/Scripts/Game/Entities/Player/PlayerMovementHandler.cs
+++ Project/Assets/Project/Scripts/Game/Entities/Player/PlayerMovementHandler.cs
@@ -9,77 +9,103 @@ public class PlayerMovementHandler : MonoBehaviour
 	private CharacterController2D controller;
 	private Vector3 velocity;
 
+	private bool canGrab = true;
+	private PlayerStatus status = PlayerStatus.Idle;
+
 	private int jumpsCount;
 	private bool canDash = true;
+	private float savedGravity;
 	private float normalizedHorizontalSpeed;
 
 	private GamepadState gamepadState;
 
+	[Tooltip("Cet index permet de choisir via quelle manette ce joueur va être controllé")]
 	[SerializeField]
-	private Index controllerIndex = Index.One;
+	private Index index = Index.One;
 
 	[Space(20)]
 
+	[Tooltip("Le transform visuel du joueur qui sera flippé selon l'axe 'LocalScaleX'. Ce n'est que visuel")]
 	[SerializeField]
 	private Transform player;
 
+	[Tooltip("Le collider du player (capsule)")]
+	[SerializeField]
+	private Collider2D trigger2D;
+
+	[Tooltip("Le point d'ancrage de la balle quand le joueur l'aura sur lui")]
+	[SerializeField]
+	private Transform ballAnchor;
+
+	[Tooltip("Le transform du pote de ce joueur")]
 	[SerializeField]
 	private Transform friendTransform;
 
+	[Tooltip("Le transform de l'ancre de la mire. Ce transform sera tourné en fonction du stick. Il ne sera pas déplacé et sert juste d'ancre")]
 	[SerializeField]
 	private Transform sightAnchor;
 
+	[Tooltip("Le transform de la mire. Ce transform sera déplacé selon l'axe X en fonction de la position du trigger L. Il doit être enfant de sightAnchor")]
 	[SerializeField]
 	private Transform sight;
 
 	[Space(20)]
 
+	[Tooltip("Permettra de faire descendre plus radipedement ou de faire planner plus longtemps le joueur")]
 	[SerializeField]
 	private float gravity = -60f;
+	[Tooltip("Vitesse du joueur")]
 	[SerializeField]
 	private float runSpeed = 13f;
-	[SerializeField]
-	private float dashSpeed = 16f;
-	[SerializeField]
-	private float dashCoolDown = 1f;
 
 	[Space(20)]
 
+	[Tooltip("Vitesse du dash. Une plus grande valeur fera parcourir une plus grand distance et plus rapidement")]
+	[SerializeField]
+	private float dashSpeed = 16f;
+	[Tooltip("Delta de temps avant que le joueur puisse refaire un Dash")]
+	[SerializeField]
+	private float dashCoolDown = 0.5f;
+	[Tooltip("Durée du dash. Une plus grande valeur fera parcourir une plus grand distance")]
+	[SerializeField]
+	private float dashDuration = 0.1f;
+	[Tooltip("Permet de parametrer le dash sur l'axe X et Y. La gravité agit sur le Y, il faut donc ajuster Y")]
+	[SerializeField]
+	private Vector2 dashDirectionOverdrive = Vector2.one;
+
+	[Space(20)]
+
+	[Tooltip("Une plus petite valeur fera glisser le joueur au sol")]
 	[SerializeField]
 	private float groundDamping = 10f;
+	[Tooltip("Une plus petite valeur fera glisser le joueur dans les air : Air control")]
 	[SerializeField]
 	private float inAirDamping = 6f;
 
 	[Space(20)]
 
+	[Tooltip("Hauteur d'un saut")]
 	[SerializeField]
 	private float jumpHeight = 4f;
+	[Tooltip("Nombre de saut autorisés avant de devoir revenir sur le sol")]
 	[SerializeField]
 	private int maxJumps = 2;
 
-	public GamepadState GamepadState
-	{
-		get
-		{
-			return this.gamepadState;
-		}
-	}
+	[Space(20)]
 
-	public Transform FriendTransform
-	{
-		get
-		{
-			return this.friendTransform;
-		}
-	}
+	[Tooltip("Delay avant de pouvoir re-grab une ball. Ne pas toucher")]
+	[SerializeField]
+	private float deltaTimeGrab = 0.2f;
 
-	public Transform Sight
-	{
-		get
-		{
-			return this.sight;
-		}
-	}
+	[Space(20)]
+
+	[Tooltip("Puissance de la balle lors d'une passe")]
+	[SerializeField]
+	private float passPower = 50f;
+
+	[Tooltip("Puissance de la balle lors d'un tir")]
+	[SerializeField]
+	private float shootPower = 50f;
 
 	public bool IsTargeting
 	{
@@ -87,62 +113,83 @@ public class PlayerMovementHandler : MonoBehaviour
 		private set;
 	}
 
-
-	public Index Index
-	{
-		get
-		{
-			return this.controllerIndex;
-		}
-	}
-
 	private void Awake()
 	{
 		this.controller = GetComponent<CharacterController2D>();
-
 		this.gamepadState = new GamepadState();
 
-		this.controller.onControllerCollidedEvent += this.OnControllerCollider;
-		this.controller.onTriggerEnterEvent += this.OnTriggerEnterEvent;
-		this.controller.onTriggerExitEvent += this.OnTriggerExitEvent;
-	}
-
-	private void OnDestroy()
-	{
-		this.controller.onControllerCollidedEvent -= this.OnControllerCollider;
-		this.controller.onTriggerEnterEvent -= this.OnTriggerEnterEvent;
-		this.controller.onTriggerExitEvent -= this.OnTriggerExitEvent;
-	}
-
-	#region Event Listeners Methods
-	private void OnControllerCollider(RaycastHit2D hit)
-	{
-		// bail out on plain old ground hits cause they arent very interesting
-		if(hit.normal.y == 1f)
+		if(this.trigger2D == null)
 		{
-			return;
+			this.trigger2D = this.GetComponent<Collider2D>();
+			Debug.Log("PlayerHandler : Awake() : trigger2D is null, try to GetComponent<Collider2D>() on it");
 		}
 
-		// logs any collider hits if uncommented. it gets noisy so it is commented out for the demo
-		//Debug.Log( "flags: " + _controller.collisionState + ", hit.normal: " + hit.normal );
+		this.trigger2D.isTrigger = true;
 	}
 
-	private void OnTriggerEnterEvent(Collider2D col)
+	private void OnTriggerEnter2D(Collider2D collision)
 	{
-		// Debug.Log("onTriggerEnterEvent: " + col.gameObject.name);
+		if(collision.CompareTag(Tags.Ball))
+		{
+			if(this.canGrab && BallHandler.Instance.Index == Index.Any)
+			{
+				BallHandler.Instance.SetGrabbed(this.ballAnchor, this.index);
+			}
+		}
+		else if(collision.CompareTag(Tags.Player2))
+		{
+
+		}
 	}
 
-	private void OnTriggerExitEvent(Collider2D col)
+	private void OnTriggerExit2D(Collider2D collision)
 	{
-		// Debug.Log("onTriggerExitEvent: " + col.gameObject.name);
+		// Debug.Log($"PlayerHandler : OnTriggerExit2D() : {collision.name}");
 	}
-	#endregion
+
 
 	private void Update()
 	{
-		ip_GamePad.GetState(ref this.gamepadState, this.controllerIndex);
+		ip_GamePad.GetState(ref this.gamepadState, this.index);
 
-		// Sight control
+		if(BallHandler.Instance.Index == this.index)
+		{
+			// Pass control
+			if(this.gamepadState.APressed)
+			{
+				if(this.IsTargeting)
+				{
+					BallHandler.Instance.Shoot(this.sight, this.passPower, ShootType.Pass);
+				}
+				else
+				{
+					BallHandler.Instance.Shoot(this.friendTransform, this.passPower, ShootType.Pass);
+				}
+
+				this.canGrab = false;
+				StartCoroutine(CoroutineUtils.DelaySeconds(() =>
+				{
+					this.canGrab = true;
+				}, this.deltaTimeGrab));
+			}
+
+			// Shoot control
+			if(this.gamepadState.BPressed)
+			{
+				if(this.IsTargeting)
+				{
+					BallHandler.Instance.Shoot(this.sight, this.shootPower, ShootType.Shoot);
+				}
+
+				this.canGrab = false;
+				StartCoroutine(CoroutineUtils.DelaySeconds(() =>
+				{
+					this.canGrab = true;
+				}, this.deltaTimeGrab));
+			}
+		}
+
+		// sight control
 		this.IsTargeting = this.gamepadState.LT > 0;
 		this.sight.transform.localPosition = (Vector3.right * this.gamepadState.LT) * 2.5f;
 
@@ -192,22 +239,44 @@ public class PlayerMovementHandler : MonoBehaviour
 			this.velocity.y = Mathf.Sqrt(2f * this.jumpHeight * -this.gravity);
 		}
 
+		// Dash control
+		if(BallHandler.Instance.Index != this.index
+			&& this.canDash
+			&& this.gamepadState.RightStickAxis.magnitude > 0.5f)
+		{
+			this.canDash = false;
+
+			var direction = new Vector2(this.gamepadState.RightStickAxis.x, this.gamepadState.RightStickAxis.y);
+			this.velocity = (direction.normalized * this.dashDirectionOverdrive) * this.dashSpeed;
+
+			this.savedGravity = this.gravity;
+			this.gravity = 0;
+
+			StartCoroutine(CoroutineUtils.DelaySeconds(() =>
+			{
+				// Reset gravity and stop dashing
+				this.gravity = this.savedGravity;
+			}, this.dashDuration));
+
+			StartCoroutine(CoroutineUtils.DelaySeconds(() =>
+			{
+				// Allow to do a dash
+				this.canDash = true;
+			}, this.dashCoolDown));
+		}
+
 		var smoothedMovementFactor = this.controller.isGrounded ? this.groundDamping : this.inAirDamping;
 		this.velocity.x = Mathf.Lerp(this.velocity.x, normalizedHorizontalSpeed * this.runSpeed, Time.deltaTime * smoothedMovementFactor);
 
-		this.velocity.y += this.gravity * Time.deltaTime;
-
-		// Dash control
-		if(this.gamepadState.XPressed && this.canDash == true)
+		if(this.gravity == 0)
 		{
-			this.canDash = false;
-			var direction = new Vector2(this.gamepadState.LeftStickAxis.x, this.gamepadState.LeftStickAxis.y + this.gravity);
-			// Fully override velocity
-			this.velocity = direction * this.dashSpeed;
-			StartCoroutine(CoroutineUtils.DelaySeconds(() =>
-			{
-				this.canDash = true;
-			}, this.dashCoolDown));
+			// Smooth dash on Y axis without gravity applied
+			this.velocity.y = Mathf.Lerp(this.velocity.y, 0, Time.deltaTime * smoothedMovementFactor);
+		}
+		else
+		{
+			// Use gravity as normal situation
+			this.velocity.y += this.gravity * Time.deltaTime;
 		}
 
 		if(this.controller.isGrounded && this.gamepadState.LeftStickAxis.y < 0)

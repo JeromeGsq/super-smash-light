1029a0f7aef8d2d7d024e45502b836ce3a6c32bf
Jérôme Ghesquiere <jerome.ghesquiere@gmail.com>
2019-01-30 19:18:38 +0100

    Revert

---
+++ Project/Assets/Plugins/Toastapp/UI/Scripts/PlayerInputInterface.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PlayerInputInterface : MonoBehaviour {
+
+    public abstract float GetAxis(string _axisName);
+
+    public abstract bool GetButtonDown(string _buttonName);
+
+}

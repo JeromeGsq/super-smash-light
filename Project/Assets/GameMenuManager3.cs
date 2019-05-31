68abb8e409582580e4050c9c20d879ddf6835ad6
fullarms <fullarms5@gmail.com>
2019-05-31 22:35:21 +0200

    menu

--- Project/Assets/GameMenuManager3.cs
+++ Project/Assets/GameMenuManager3.cs
@@ -15,15 +15,12 @@ public class GameMenuManager3 : MonoBehaviour
     [SerializeField]
     private Vector3 pos1;
 
-    [Space(20)]
     [SerializeField]
     private Vector3 pos2;
 
-    [Space(20)]
     [SerializeField]
     private Vector3 pos3;
 
-    [Space(20)]
     [SerializeField]
     private Vector3 pos4;
 
@@ -34,60 +31,105 @@ public class GameMenuManager3 : MonoBehaviour
     [Space(20)]
     [SerializeField]
     private GameObject bigPreview;
+    [Space(20)]
+    [SerializeField]
+    private Sprite lvlA;
+    [SerializeField]
+    private Sprite lvl1;
+    [SerializeField]
+    private Sprite lvl2;
+    [SerializeField]
+    private Sprite lvl3;
 
+    [Space(20)]
+    [SerializeField]
+    private bool pressed;
     // Start is called before the first frame update
     void Start()
     {
         position = 1;
         this.gamepadState = new GamepadState();
+        pressed = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         ip_GamePad.GetState(ref this.gamepadState, ip_GamePad.Index.Any);
-
+        pressed = false;
 
         if(this.position == 4) 
             {
-            if(this.gamepadState.Right) 
+            if(this.gamepadState.RightPressed && pressed == false) 
             {
                 this.position = 1;
+                pressed = true;
             }
-        }
+            
+            if(this.gamepadState.LeftPressed && pressed == false)
+                {
+                    this.position = 3;
+                    pressed = true;
+                }
+            }
         if(this.position == 3)
         {
-            if(this.gamepadState.Right)
+            if(this.gamepadState.RightPressed && pressed == false)
             {
                 this.position = 4;
+                pressed = true;
+            }
+            if(this.gamepadState.LeftPressed && pressed == false) 
+            {
+                this.position = 2;
+                pressed = true;
             }
         }
         if(this.position == 2) 
         {
-            if(this.gamepadState.Right) 
+            if(this.gamepadState.RightPressed && pressed == false) 
             {
                 this.position = 3;
+                pressed = true;
+            }
+            if(this.gamepadState.LeftPressed && pressed == false) 
+            {
+                this.position = 1;
+                pressed = true;
             }
         }
         if(this.position == 1) 
         {
-            if(this.gamepadState.Right) 
+            if(this.gamepadState.RightPressed && pressed == false) 
             {
                 this.position = 2;
+                pressed = true;
+            }
+            if(this.gamepadState.LeftPressed && pressed == false) 
+            {
+                this.position = 4;
+                pressed = true;
             }
         }
 
         if(this.position == 1) {
             cursor.GetComponent<Transform>().position = pos1;
+            bigPreview.GetComponent<SpriteRenderer>().sprite = lvlA;
         }
         if(this.position == 2) {
             cursor.GetComponent<Transform>().position = pos2;
+            bigPreview.GetComponent<SpriteRenderer>().sprite = lvl1;
+
         }
         if(this.position == 3) {
             cursor.GetComponent<Transform>().position = pos3;
+            bigPreview.GetComponent<SpriteRenderer>().sprite = lvl2;
+
         }
         if(this.position == 4) {
             cursor.GetComponent<Transform>().position = pos4;
+            bigPreview.GetComponent<SpriteRenderer>().sprite = lvl3;
+
         }
     }
 }
